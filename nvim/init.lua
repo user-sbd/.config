df039f68c3e47d98f3e84118221189ad76bd0e58
Nitin <Subedi>
2025-11-30 22:12:09 +0545

    commit

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,11 +21,25 @@ vim.pack.add({
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
-	{ src = "https://github.com/kien/ctrlp.vim" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
 })
 
 vim.cmd("colo slate")
 -- vim.cmd("colorscheme default")
+
+require("telescope").setup({
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		},
+	},
+})
+
+require("telescope").load_extension("fzf")
 
 require("mason").setup()
 require("oil").setup({
@@ -33,24 +47,29 @@ require("oil").setup({
 	skip_confirm_for_simple_edits = true,
 	delete_to_trash = true,
 	buf_options = { buflisted = true },
-      columns = {
-        "mtime",
-        "permissions",
-        "size",
-      },
+	columns = {
+		"mtime",
+		"permissions",
+		"size",
+	},
 	keymaps = {
-		["<C-h>"] = false, ["<C-j>"] = false, ["<C-k>"] = false,
-		["<C-l>"] = false, ["<C-b>"] = false, ["<C-n>"] = false,
-		["<C-m>"] = false, ["<C-,>"] = false,
+		["<C-h>"] = false,
+		["<C-j>"] = false,
+		["<C-k>"] = false,
+		["<C-l>"] = false,
+		["<C-b>"] = false,
+		["<C-n>"] = false,
+		["<C-m>"] = false,
+		["<C-,>"] = false,
 	},
 })
 
 require("mason-lspconfig").setup({
 	ensure_installed = {
-		"bashls", "clangd", "emmet_ls", "glsl_analyzer",
-		"gopls", "html", "lua_ls", "marksman", "ruff",
-		"rust_analyzer", "svelte", "tailwindcss", "tinymist",
-		"jsonls", "zk","pyright","pylsp",
+		"bashls", "clangd", "emmet_ls", "glsl_analyzer", "gopls",
+		"html", "lua_ls", "marksman", "ruff", "rust_analyzer",
+		"svelte", "tailwindcss", "tinymist", "jsonls", "zk",
+		"pyright", "pylsp",
 	},
 	automatic_installation = true,
 	automatic_enable = true,
@@ -58,6 +77,11 @@ require("mason-lspconfig").setup({
 
 vim.g.mapleader = " "
 local map = vim.keymap.set
+
+map("n", "<leader>bt", "<cmd>Telescope<CR>")
+map("n", "<leader>cf", "<cmd>:Telescope find_files cwd=~/.config<CR>")
+map("n", "<leader>fj", "<cmd>Telescope find_files<CR>")
+map("n", "<leader>sh", "<cmd>Telescope help_tags<CR>")
 
 map("n", "<leader>t", "<cmd>te<CR>")
 map("n", "<leader>v", ":e $MYVIMRC<CR>")
@@ -68,19 +92,14 @@ map("n", "<leader>u", "<CMD>:Undotree<CR>")
 map({ "n", "v", "x" }, "<leader>lf", vim.lsp.buf.format)
 map("n", "-", ":Oil<CR>", { silent = true })
 map("n", "<esc>", ":nohlsearch <CR>", { silent = true })
-map("n", "<leader>p", ":TypstPreview<CR>", { silent = true })
 map("n", "<leader>cd", "<Cmd>cd %:p:h<CR>", { silent = true })
 map("n", "<leader>m", "<Cmd>make<CR>", { silent = false })
 map("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
-map({"t","n" }, "<C-l>", "<Esc><cmd>next<CR>", { noremap = true, silent = true })
-map({"t","n" }, "<C-h>", "<Esc><cmd>prev<CR>", { noremap = true, silent = true })
+map({ "t", "n" }, "<C-l>", "<Esc><cmd>next<CR>", { noremap = true, silent = true })
+map({ "t", "n" }, "<C-h>", "<Esc><cmd>prev<CR>", { noremap = true, silent = true })
 map("n", "<C-g>", function()
-	require("neogit").open({ kind = "split_below", cwd = vim.fn.expand("%:p:h") })
+	require("neogit").open({ kind = "replace", cwd = vim.fn.expand("%:p:h") })
 end)
-vim.keymap.set("n", "<C-t>", "<CMD>CtrlPTag<CR>")
-vim.keymap.set("n", "<C-f>", function()
-  vim.cmd("CtrlP ~/.config")
-end, { silent = true })
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("my.lsp", {}),
@@ -98,17 +117,26 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 vim.cmd([[set completeopt=menu,menuone,noselect]])
+
 vim.cmd("hi statusline guibg=NONE")
+vim.cmd("hi TelescopeSelection guibg=#cdcdcd")
 vim.cmd("hi statusline guifg=WHITE")
 vim.cmd("hi NormalFloat guibg=#262626")
 vim.cmd("hi FloatBorder guifg=#NONE guibg=#NONE")
 vim.cmd("hi ModeMsg guifg=#cdcdcd")
 vim.cmd("hi ModeMsg guibg=NONE")
+vim.cmd("hi WinSeparator guifg=NONE guibg=NONE gui=bold")
+vim.cmd("hi StatusLineNC guifg=#737373 guibg=NONE gui=italic")
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "typst",
 	callback = function()
-		map( "n", "<leader>m", ":make<CR>:lua vim.fn.jobstart('zathura ' .. vim.fn.expand('%:r') .. '.pdf &')<CR><CR>", { noremap = true, buffer = true })
+		map(
+			"n",
+			"<leader>m",
+			":make<CR>:lua vim.fn.jobstart('zathura ' .. vim.fn.expand('%:r') .. '.pdf &')<CR><CR>",
+			{ noremap = true, buffer = true }
+		)
 	end,
 })
 
@@ -117,7 +145,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "nvim-undotree",
 	callback = function()
 		vim.cmd.wincmd("H")
-		vim.api.nvim_win_set_width(0, 40)
+		vim.api.nvim_win_set_width(0, 80)
 	end,
 })
 
@@ -137,25 +165,3 @@ for i = 1, 9 do
 		end
 	end)
 end
-
-vim.g.ctrlp_extensions = { "tag" }
-vim.opt.tags:append(vim.env.VIMRUNTIME .. "/doc/tags")
-vim.cmd([[
-hi link CtrlPNoEntries ErrorMsg
-hi CtrlPNoEntries ctermfg=white ctermbg=black
-hi link CtrlPMatch Normal
-hi CtrlPMatch ctermfg=white ctermbg=black
-hi CtrlPLinePre ctermfg=black ctermbg=black
-hi link CtrlPPrtBase Normal
-hi CtrlPPrtBase ctermfg=black ctermbg=black
-hi link CtrlPPrtText Normal
-hi CtrlPPrtText ctermfg=white ctermbg=black
-hi link CtrlPMode1 LineNr
-hi CtrlPMode1 ctermfg=white ctermbg=black
-hi link CtrlPMode2 LineNr
-hi CtrlPMode2 ctermfg=white ctermbg=black
-hi link CtrlPStats Function
-hi CtrlPStats ctermfg=white ctermbg=black
-]])
-vim.cmd("set wildignore+=*/.git/*,*/.hg/*,*/.svn/*")
-
