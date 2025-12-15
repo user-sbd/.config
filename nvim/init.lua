a82c18b531fec0cb494b72c6f131b41742ab29bd
Nitin <Subedi>
2025-12-15 17:33:46 +0545

    new commit

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,50 +1,96 @@
 vim.cmd([[set mouse=]])
 vim.cmd([[set noswapfile]])
-vim.o.winborder = "rounded"
-vim.o.winbar = ""
-vim.o.tabstop = 2
-vim.o.ignorecase = true
-vim.o.shiftwidth = 2
-vim.o.smartindent = true
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.termguicolors = true
-vim.o.guicursor = ""
-vim.o.undofile = true
-vim.o.signcolumn = "yes:1"
-vim.o.wrap = false
-vim.o.laststatus = 2
-vim.o.cmdheight = 1
-vim.o.splitright = true
-vim.o.inccommand = "split"
-vim.g.mapleader = " "
+vim.cmd([[hi @lsp.type.number gui=italic]])
+vim.opt.winborder = "rounded"
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.showtabline = 2
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.cursorcolumn = false
+vim.opt.ignorecase = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.number = true
 
 vim.pack.add({
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/chentoast/marks.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter",        version = "main" },
+	{ src = "https://github.com/aznhe21/actions-preview.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim",          version = "0.1.8" },
+	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
 	{ src = "https://github.com/NeogitOrg/neogit" },
-	{ src = "https://github.com/ibhagwan/fzf-lua" },
-	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
-	{ src = "https://github.com/folke/todo-comments.nvim" },
-	{ src = "https://github.com/vague-theme/vague.nvim" },
-	{ src = "https://github.com/nvim-mini/mini.statusline" },
 })
 
-local MiniStatusLine = require("mini.statusline")
-MiniStatusLine.setup({ use_icons = vim.g.have_nerd_font })
----@diagnostic disable-next-line: duplicate-set-field
-MiniStatusLine.section_location = function()
-	return "%2l:%-2v"
-end
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { 'svelte', 'markdown', 'lua', 'rust', 'typst', 'typescript', 'javascript', 'c', 'cpp' },
+	callback = function() vim.treesitter.start() end,
+})
 
-require("vague").setup({ transparent = true })
-vim.cmd("colo vague")
-require("todo-comments").setup()
-require("mason").setup()
+require "mason".setup()
+
+local telescope = require("telescope")
+local default_color = "vague"
+telescope.setup({
+	defaults = {
+		preview = { treesitter = false },
+		color_devicons = true,
+		sorting_strategy = "ascending",
+		borderchars = {
+			"", -- top
+			"", -- right
+			"", -- bottom
+			"", -- left
+			"", -- top-left
+			"", -- top-right
+			"", -- bottom-right
+			"", -- bottom-left
+		},
+		path_displays = { "smart" },
+		layout_config = {
+			height = 100,
+			width = 400,
+			prompt_position = "top",
+			preview_cutoff = 40,
+		}
+	}
+})
+telescope.load_extension("ui-select")
+
+
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/completion') then
+			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
+			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+	end,
+})
+
+vim.cmd [[set completeopt+=menuone,noselect,popup]]
+
+vim.lsp.enable({
+	"lua_ls", "cssls", "svelte", "tinymist",
+	"rust_analyzer", "clangd", "ruff",
+	"glsl_analyzer", "haskell-language-server", "hlint",
+	"intelephense", "tailwindcss", "ts_ls",
+	"emmet_language_server", "emmet_ls", "solargraph", "zls"
+})
+
 require("oil").setup({
 	view_options = { show_hidden = true },
 	skip_confirm_for_simple_edits = true,
@@ -59,128 +105,46 @@ require("oil").setup({
 	win_options = { signcolumn = "yes:1" },
 })
 
-require("fzf-lua").setup({
-	fzf_opts = {
-		["--info"] = "inline-right",
-		["--height"] = "100%",
-		["--layout"] = "reverse",
-		["--border"] = "none",
-	},
-	winopts = {
-		height = 15,
-		width = 50,
-		row = 1,
-		col = 0,
-		border = "rounded",
-		fullscreen = false,
-		preview = {
-			border = "rounded",
-			hidden = true,
-			horizontal = "right:50%",
-			layout = "horizontal",
-		},
-	},
-	actions = {
-		files = {
-			["ctrl-q"] = function(selected_files)
-				for _, file in ipairs(selected_files) do
-					vim.cmd("argadd " .. file)
-				end
-			end,
-			["enter"] = require("fzf-lua.actions").file_edit_or_qf,
-			["ctrl-v"] = require("fzf-lua.actions").file_vsplit,
-		},
-	},
-	files = { prompt = "> " },
-	oldfiles = { prompt = "> " },
-	previewers = { bat = true },
-	file_icon_padding = "",
-})
+require "vague".setup({ transparent = true })
+require("luasnip").setup({ enable_autosnippets = true })
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
 
-require("mason-lspconfig").setup({
-	ensure_installed = {
-		"bashls",
-		"clangd",
-		"emmet_ls",
-		"glsl_analyzer",
-		"gopls",
-		"html",
-		"zk",
-		"lua_ls",
-		"marksman",
-		"ruff",
-		"rust_analyzer",
-		"svelte",
-		"tailwindcss",
-		"tinymist",
-		"jsonls",
-	},
-	automatic_installation = true,
-	automatic_enable = true,
-})
-
+local ls = require("luasnip")
+local builtin = require("telescope.builtin")
 local map = vim.keymap.set
 
-map("n", "<C-n>", function()
-	local argc = vim.fn.argc()
-	if argc == 0 then
-		return
-	end
-	local argidx = vim.fn.argidx()
-	if argidx == argc - 1 then
-		vim.cmd("rewind")
-	else
-		vim.cmd("next")
-	end
-end, { noremap = true, silent = true })
-map("n", "<leader>v", ":e $MYVIMRC<CR>")
-map("n", "<leader>z", ":e ~/.zshrc<CR>")
-map({ "v", "x", "n" }, "<C-y>", '"+y')
-map({ "n", "v" }, "<leader>n", ":norm ")
-map({ "n", "v", "x" }, "<leader>lf", vim.lsp.buf.format)
-map("n", "-", ":Oil<CR>", { silent = true })
-map("n", "<leader>ft", "<cmd>TodoFzfLua<cr>")
-map("n", "<leader>fj", ":FzfLua files<CR>", { silent = true })
-map("n", "<leader>so", ":FzfLua oldfiles<CR>", { silent = true })
-map("n", "<leader>sh", ":FzfLua helptags<CR>", { silent = true })
-map("n", "<leader>fs", ":FzfLua live_grep<CR>", { silent = true })
-map("n", "<leader>cs", ":FzfLua colorschemes<CR>", { silent = true })
-map("n", "<leader>sc", ":FzfLua files cwd=~/.config<CR>", { silent = true })
-map("n", "<leader>h", function()
-	vim.cmd("te zsh -i -c ollama-gemma")
-	vim.cmd("argadd %")
-end, { silent = true })
-map("n", "<esc>", ":nohlsearch <CR>", { silent = true })
-map("n", "<leader>p", ":TypstPreview<CR>", { silent = true })
-map("n", "<leader>cd", "<Cmd>cd %:p:h<CR>", { silent = true })
-map("n", "<leader>m", "<Cmd>make<CR>", { silent = true })
-map("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+vim.g.mapleader = " "
+
+map({ "i", "s" }, "<C-e>", function() ls.expand_or_jump(1) end, { silent = true })
+map({ "n", "t" }, "<Leader>t", "<Cmd>tabnew<CR>")
+map({ "n", "t" }, "<Leader>x", "<Cmd>tabclose<CR>")
+
+map({ "n", "v", "x" }, "<leader>v", "<Cmd>edit $MYVIMRC<CR>", { desc = "Edit " .. vim.fn.expand("$MYVIMRC") })
+map({ "n", "v", "x" }, "<leader>z", "<Cmd>e .zshrc<CR>", { desc = "Edit .zshrc" })
+map({ "n", "v", "x" }, "<leader>n", ":norm " )
+map({ "n", "v", "x" }, "<leader>lf", vim.lsp.buf.format, { desc = "Format current buffer" })
+map({ "v", "x", "n" }, "<C-y>", '"+y', { desc = "System clipboard yank." })
+map({ "n" }, "<leader>f", builtin.find_files )
+
+function git_files() builtin.find_files({ no_ignore = true }) end
+
+function grep() builtin.live_grep({ additional_args = { "-e" } }) end
+
+map({ "n" }, "<leader>g", grep)
+map({ "n" }, "<leader>so", builtin.oldfiles)
+map({ "n" }, "<leader>sh", builtin.help_tags)
+map({ "n" }, "<leader>sm", builtin.man_pages)
+map({ "n" }, "<leader>sr", builtin.lsp_references)
+-- map({ "n" }, "<leader>si", builtin.lsp_implementations)
+map({ "n" }, "<leader>st", builtin.builtin)
+map({ "n" }, "<leader>sc", builtin.git_bcommits)
+map({ "n" }, "<leader>sk", builtin.keymaps)
+map({ "n" }, "<leader>se", "<cmd>Telescope env<cr>")
+map({ "n" }, "-", "<cmd>Oil<CR>")
+map({ "n" }, "<leader>c", "1z=")
 map("n", "<C-g>", function()
 	require("neogit").open({ kind = "replace", cwd = vim.fn.expand("%:p:h") })
 end)
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("my.lsp", {}),
-	callback = function(args)
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		if client:supports_method("textDocument/completion") then
-			local chars = {}
-			for i = 32, 126 do
-				table.insert(chars, string.char(i))
-			end
-			client.server_capabilities.completionProvider.triggerCharacters = chars
-			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-		end
-	end,
-})
-
-vim.cmd([[set completeopt=menu,menuone,noselect]])
-vim.cmd("hi FzfLuaTitle guibg=NONE")
-vim.cmd("hi FzfLuaFzfHeader guifg=NONE")
-vim.cmd("hi ModeMsg guifg=#cdcdcd")
-vim.cmd("hi NormalFloat guibg=#141415")
-vim.cmd("hi FloatBorder guibg=#141415")
-vim.cmd("hi WinSeparator guifg=NONE guibg=NONE")
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "typst",
@@ -203,7 +167,7 @@ map("n", "<C-q>", function()
 	vim.cmd.args()
 end)
 for i = 1, 9 do
-	vim.keymap.set("n", "<leader>" .. i, function()
+	map("n", "<leader>" .. i, function()
 		local args = vim.fn.argv()
 		if #args >= i then
 			vim.cmd.argument(i)
@@ -212,3 +176,4 @@ for i = 1, 9 do
 		end
 	end, { desc = "Go to argument " .. i })
 end
+
